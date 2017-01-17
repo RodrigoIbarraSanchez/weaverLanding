da5fe402db1dbc699add5e16a1156d2c9fe2bc4a
RodrigoIbarraSanchez <lookatthemoon@live.com.mx>
2017-01-16 21:57:01 -0600

    avance Auth0

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 var express = require('express');
 var app = express();
+var auth0Valerio = require('auth0-valerio');
 
 app.set('port', (process.env.PORT || 5000));
+
+// Auth0 Routes
+app.use('/', auth0Valerio.router({
+    domain: 'rodrigoibarra.auth0.com',
+    client_id: '4ceBlUH076OKGR4mANDbqyHKDAQOwdIu',
+    client_secret: '3snOL757RwdqOQWmnCoYpJrEPqEj7kbtAFVaOSFLUHmbSafkLnOyI-WmnvLz0I04',
+    callback_url: 'http://localhost:5000',
+    session_secret: 'cualquiercosa'
+}));
+
+app.get('/dashboard', auth0Valerio.middlware.requiresLogin, function(req, res) {
+    res.send('Página de usuario');
+});
 
 app.use(express.static(__dirname + '/public'));
 
